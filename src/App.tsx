1528fd85efe2baf91fbe96fba8c9efb33159628c
cotne998 <cotnecincadze998@gmail.com>
2025-05-06 15:01:03 +0400

    app.tsx commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 const Home = React.lazy(() => import("./components/Home/Home"));
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
         {/* <Route path='/Login' element={<Login />} /> */}
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
