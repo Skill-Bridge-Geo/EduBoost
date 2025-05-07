016d88c72cd17a92898e98c1b3346df1ba4c58c0
Tailung1 <ramazsamitashvili11@gmail.com>
2025-05-07 17:42:08 +0400

    paid video page's mobile resolution's first part done

--- src/components/Paid/Video/Video.tsx
+++ src/components/Paid/Video/Video.tsx
@@ -1,10 +1,18 @@
+import "./video.css";
 import { useEffect, useState } from "react";
-import axios from "axios";
-
 import { CourseData } from "../../../types"; // I
+
+import axios from "axios";
+import connectionIocn from "../../../assets/Cellular Connection.png";
+import wifiIcon from "../../../assets/Wifi.svg";
+import batteryIcon from "../../../assets/Battery.svg";
+import checkedIcon from "../../../assets/checked.svg";
+import signedIcon from "../../../assets/signed.svg";
+import Header from "../../Header/Header";
 
 export default function Video() {
   const [data, setData] = useState<CourseData | null>(null);
+
 
   useEffect(() => {
     axios
@@ -16,8 +24,55 @@ export default function Video() {
   if (!data) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>{data.course.title}</h1>
+    <div className='parent'>
+      <section className='first-child'>
+        <div className='header'>
+          <Header />
+        </div>
+        <div className='mobileTools'>
+          <p>9:41</p>
+          <div className='tools'>
+            <img src={connectionIocn} alt='connection Iocn' />
+            <img src={wifiIcon} alt='wifi Icon' />
+            <img src={batteryIcon} alt='battery Icon' />
+          </div>
+        </div>
+        <div className='video-container'>
+          <iframe
+            width='100%'
+            height='212px'
+            src={data.course.video.url}
+            title='Course Video'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <section className='course-raitings'>
+          <h2>{data.course.title}</h2>
+          <div className='studio-container'>
+            <div className='studioName'>
+              <p>Kitani Studio</p>
+              <span>Design Studio</span>
+            </div>
+            <div className='detailed-raitings'>
+              <div className='check'>
+                <img src={checkedIcon} alt='people icon' />
+                <span>{data.course.views}</span>
+              </div>
+              <div className='sign'>
+                {" "}
+                <img src={signedIcon} alt='sign icon' />
+                <span>{data.course.likes}</span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section className='second-child'>
+      
+        <h1>axsasx</h1>
+      </section>
     </div>
   );
 }
