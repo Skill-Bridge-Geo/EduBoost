ad6186048c84ef8f17c6bbbd1e2f234c597f0372
Tailung1 <ramazsamitashvili11@gmail.com>
2025-05-11 01:30:47 +0400

    set up video status indicator

--- src/components/Paid/ChapterDetails/ChapterDetails.tsx
+++ src/components/Paid/ChapterDetails/ChapterDetails.tsx
@@ -5,12 +5,26 @@ interface g {
 }
 export default function ChapterDetails({ chapter }: g) {
   return (
-    <div>
-      {chapter.videos.map((video) => (
-        <div className='videos'>
+    <div className='main'>
+      {chapter.videos.map((video, index) => (
+        <div className='video-wrapper' key={index}>
           <div className='status-conatiner'>
-            <p className="title">{video.title}</p>
-            <p className="status">{video.status}</p>
+            <p className='title'>{video.title}</p>
+            <div
+              className={
+                video.status === "completed"
+                  ? "completed"
+                  : video.status === "playing"
+                  ? "uncompleted"
+                  : "locked"
+              }
+            >
+              {video.status === "completed"
+                ? "Completed"
+                : video.status === "playing"
+                ? "Playing"
+                : ""}
+            </div>
           </div>
           <hr className='hr' />
         </div>
