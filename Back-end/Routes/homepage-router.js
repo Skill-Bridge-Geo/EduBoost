d2a0f1d8ac619b7fee9bb0bdf63fb640f464122e
Luka <lussskki@gmail.com>
2025-05-14 17:45:35 +0400

    added update and delete method

--- Back-end/Routes/homepage-router.js
+++ Back-end/Routes/homepage-router.js
@@ -2,12 +2,17 @@ import express from 'express'
 import { homePageController } from '../Controllers/homepage-controller.js'
 import { allStudiosCourses } from '../Controllers/homepage-controller.js'
 import { addCourse } from '../Controllers/homepage-controller.js'
+import { updateCourseById } from '../Controllers/homepage-controller.js'
+import { deleteCourseById } from '../Controllers/homepage-controller.js'
 
 const homePageRouter = express.Router()
 
 homePageRouter.post('/', homePageController)
 homePageRouter.post('/allStudio', allStudiosCourses )
 homePageRouter.post('/addToStudio', addCourse )
+homePageRouter.put('/updateCourse/:id', updateCourseById)
+homePageRouter.delete('/deleteCourse/:id', deleteCourseById)
+
 
 
 export default homePageRouter
