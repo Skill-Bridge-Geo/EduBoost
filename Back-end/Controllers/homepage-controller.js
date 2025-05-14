1dc589599c3c4dd9c157655a770f0bf663a8df85
Luka <lussskki@gmail.com>
2025-05-14 17:47:08 +0400

    added update and delete method logic

--- Back-end/Controllers/homepage-controller.js
+++ Back-end/Controllers/homepage-controller.js
@@ -25,3 +25,38 @@ export const addCourse = async (req, res) => {
         res.status(400).json({error: 'Failed to add course', details: error.message})
     }
 }
+
+export const updateCourseById = async (req, res) => {
+  try {
+    console.log('working update')
+    /* Update course by given id*/
+    const updatedCourse = await allCourseSchema.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+
+    if (!updatedCourse) {
+      return res.status(404).json({ message: 'Course not found' })
+    }
+
+    res.status(200).json(updatedCourse)
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating course', error })
+  }
+}
+
+export const deleteCourseById = async (req, res) => {
+  try {
+    /* Delete course by given id */
+    const deletedCourse = await allCourseSchema.findByIdAndDelete(req.params.id)
+
+    if (!deletedCourse) {
+      return res.status(404).json({ message: 'Course not found' })
+    }
+
+    res.status(200).json({ message: 'Course deleted successfully', data: deletedCourse })
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting course', error })
+  }
+}
