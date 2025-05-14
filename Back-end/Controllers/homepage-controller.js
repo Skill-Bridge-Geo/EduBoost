9fa5da0d4176471ca2c6d47f0a004f917ad1758f
Luka <lussskki@gmail.com>
2025-05-14 16:45:54 +0400

    created new features like get all courses and add new

--- Back-end/Controllers/homepage-controller.js
+++ Back-end/Controllers/homepage-controller.js
@@ -1,3 +1,27 @@
+import allCourseSchema from "../Models/all-courses-schema.js" // kitani studio courses schema
+
 export const homePageController = async (req, res) => {
-    console.log('homepage-controller is working')
+    console.log('Home Page Controller is working')
+}
+
+export const allStudiosCourses = async (req, res) => {
+    try {
+        /*Show all Studio courses */
+        const courses = await allCourseSchema.find() // fetch all
+        res.json(courses)
+    } catch (error) {
+        res.status(500).json({ error: 'Server error'})
+    }
+    
+}
+
+export const addCourse = async (req, res) => {
+    try {
+        /* Add course to database*/
+        const newCourse = new allCourseSchema(req.body)
+        await newCourse.save()
+        res.status(201).json({message: 'Course added', data: newCourse})
+    } catch (error) {
+        res.status(400).json({error: 'Failed to add course', details: error.message})
+    }
 }
