89d8f8c44bd5974dd3a2efe5adf7bd01b569e29c
Luka <lussskki@gmail.com>
2025-05-14 16:41:45 +0400

    added homepage routes from homepage controller

--- Back-end/Routes/homepage-router.js
+++ Back-end/Routes/homepage-router.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import { homePageController } from '../Controllers/homepage-controller.js'
+import { allStudiosCourses } from '../Controllers/homepage-controller.js'
+import { addCourse } from '../Controllers/homepage-controller.js'
 
 const homePageRouter = express.Router()
 
 homePageRouter.post('/', homePageController)
+homePageRouter.post('/allStudio', allStudiosCourses )
+homePageRouter.post('/addToStudio', addCourse )
+
 
 export default homePageRouter
