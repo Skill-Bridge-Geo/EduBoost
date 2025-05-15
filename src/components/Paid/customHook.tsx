f2d2f84222bce6fd2e4768b16a9e4d234401cedc
Tailung1 <ramazsamitashvili11@gmail.com>
2025-05-15 16:45:21 +0400

    create customHook to get data

---
+++ src/components/Paid/customHook.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {CourseData} from "../../types"
+
+export const useFetchPaidData = () => {
+  const [data, setData] = useState<CourseData| null>(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/paid.json")
+      .then((res) => setData(res.data))
+      .catch((err) => setError(err));
+  }, []);
+  return { data, error };
+};
