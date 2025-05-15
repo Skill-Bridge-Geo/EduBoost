23638fb79c0b84f0fd848cea3132ea631e6531f3
Luka <lussskki@gmail.com>
2025-05-15 15:49:19 +0400

    schema for instructors

---
+++ Back-end/Models/instructor-schema.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const instructorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  title: { type: String, required: true },
+  imageUrl: { type: String, required: true }
+})
+
+const Instructor = mongoose.model('Instructors', instructorSchema)
+export default Instructor
