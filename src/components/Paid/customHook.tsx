4c13447cca5422efe96e5774eaa886be62ab59db
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2025-05-16 16:34:19 +0400

    fixed errors from netlify 2

--- src/components/Paid/customHook.tsx
+++ src/components/Paid/customHook.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {CourseData} from "../../types"
+import { CourseData } from "../../types";
 
 export const useFetchPaidData = () => {
-  const [data, setData] = useState<CourseData| null>(null);
+  const [data, setData] = useState<CourseData | null>(null);
   const [error, setError] = useState(null);
   useEffect(() => {
     axios
       .get("/paid.json")
       .then((res) => setData(res.data))
-      .catch((err) => setError(err));
-  }, []);
+      .catch((err) => {
+        setError(err);
+        console.log(error);
+      });
+  }, [error]);
   return { data };
 };
