ad61c800ea86698b7416f378809774765ceb2ad9
Luka <lussskki@gmail.com>
2025-05-16 16:05:37 +0400

    upt logout cookies

--- Back-end/Controllers/logout-controller.js
+++ Back-end/Controllers/logout-controller.js
@@ -1,8 +1,9 @@
 export const logoutUser = (req, res) => {
     res.clearCookie('refreshToken', {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'Strict'
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
+    path: '/',
     })
     return res.status(200).json({ message: 'Logged out successfully' })
 }
