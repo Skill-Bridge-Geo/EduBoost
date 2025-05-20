847ef9c29d43b822786356c96714fd2fc45b85ca
Tailung1 <ramazsamitashvili11@gmail.com>
2025-05-20 20:23:47 +0400

    feat:add 'Load More' functionality to reviews section

--- src/components/Paid/Video/Review.tsx
+++ src/components/Paid/Video/Review.tsx
@@ -1,23 +1,26 @@
 import { useFetchPaidData } from "../customHook";
 
-export default function Review() {
+export default function Review({ loadMore }: { loadMore: boolean }) {
   const { data } = useFetchPaidData();
+  const reviewsToDsiplay = data?.reviews.slice(0, 4);
   return (
     <div className='review-container'>
-      {data?.reviews.map((review) => (
-        <div className='avatar-wrapper'>
-          <img
-            className='avatar-icon'
-            src={review.avatar}
-            alt='avatar icon'
-          />
-          <div className='avatar-details'>
-            <h2 className='avatar-name'>{review.name}</h2>{" "}
-            <span className='avatar-time'>{review.time}</span>
-            <p className='avatar-comment'>{review.comment}</p>
+      {(loadMore ? data?.reviews : reviewsToDsiplay)?.map(
+        (review) => (
+          <div className='avatar-wrapper'>
+            <img
+              className='avatar-icon'
+              src={review.avatar}
+              alt='avatar icon'
+            />
+            <div className='avatar-details'>
+              <h2 className='avatar-name'>{review.name}</h2>{" "}
+              <span className='avatar-time'>{review.time}</span>
+              <p className='avatar-comment'>{review.comment}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 }
